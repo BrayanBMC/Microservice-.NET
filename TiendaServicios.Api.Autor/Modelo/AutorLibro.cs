e3de4d96b3c11e6f6b31e3fb4b6448c195469c11
BrayanBmc <bacostap@udistrital.edu.co>
2024-10-29 08:56:31 -0500

    Modelo

---
+++ TiendaServicios.Api.Autor/Modelo/AutorLibro.cs
@@ -0,0 +1,16 @@
+﻿namespace TiendaServicios.Api.Autor.Modelo
+{
+    public class AutorLibro
+    {
+        public int AutorLibroid { get; set; }
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public DateTime? FechaDeNacimiento { get; set; }
+
+        public ICollection<GradoAcademico> ListaGradoAcademico { get; set; }
+
+        public string AutorLibroGuid { get; set; }
+    }
+}
---
+++ TiendaServicios.Api.Autor/Modelo/GradoAcademico.cs
@@ -0,0 +1,14 @@
+﻿namespace TiendaServicios.Api.Autor.Modelo
+{
+    public class GradoAcademico
+    {
+        public int GradoAcademicoid { get;set; }
+        public string Nombre { get; set; }
+        public string CentroAcademico { get; set; }
+        public DateTime? FechaGrado { get; set; }
+        public int AutorLibroid { get; set; }
+        public AutorLibro AutorLibro { get; set; }
+        public string GradoAcademicoGuid { get; set; }
+        
+    }
+}
