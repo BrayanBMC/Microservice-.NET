a770b1e381bc75ac7db774b09cad97352aa51cdb
BrayanBmc <bacostap@udistrital.edu.co>
2024-11-01 14:14:46 -0500

    AplicacionCQRS

---
+++ TiendaServicios.Api.Autor/Aplicacion/Nuevo.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using System.Data;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            public readonly ContextoAutor _contexto;
+
+            public Manejador(ContextoAutor contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var autorlibro = new AutorLibro
+                {
+                    Nombre = request.Nombre,
+                    FechaDeNacimiento = request.FechaNacimiento,
+                    Apellido=request.Apellido
+                };
+
+                _contexto.AutorLibro.Add(autorlibro);
+                var valor= await _contexto.SaveChangesAsync();
+
+                if (valor > 0)
+                {
+                    return Unit.Value;
+                }
+                throw new Exception("No se pudo insertar el autor del libro");
+            }
+
+            Task IRequestHandler<Ejecuta>.Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- TiendaServicios.Api.Autor/obj/Debug/net8.0/TiendaServicios.Api.Autor.AssemblyInfo.cs
+++ TiendaServicios.Api.Autor/obj/Debug/net8.0/TiendaServicios.Api.Autor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaServicios.Api.Autor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3de4d96b3c11e6f6b31e3fb4b6448c195469c11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12daddfce183fab13afa737591b383203476272")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaServicios.Api.Autor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaServicios.Api.Autor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
